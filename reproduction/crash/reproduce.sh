48302d8157a7bcaf43bd7064701bf88702d980f9
Kevin Rosendahl <kevindrosendahl@gmail.com>
2019-06-03 21:09:35 -0700

    add crash reproduction

---
+++ reproduction/crash/reproduce.sh
@@ -0,0 +1,28 @@
+killall iperf
+sudo pkill cubic
+
+pushd ../..
+echo "Making ccp"
+make
+
+echo "Enabling ccp"
+./scripts/setup.sh ipc=0
+
+echo "Starting cubic ccp implementation, logging to ~/crash-cubic-ccp.log"
+sudo ./generic-cong-avoid/target/release/cubic --ipc=netlink --deficit_timeout=20 > ~/crash-cubic-ccp.log 2>&1 &
+
+sleep 1
+
+echo "Starting iperf server, logging to ~/crash-iperf-server.log"
+iperf -s -p 4242 > ~/crash-iperf-server.log 2>&1 &
+
+echo "Starting iperf client, logging to ~/crash-iperf-client.log"
+iperf -c localhost -p 4242 -Z ccp -t 1 2>&1 | tee ~/crash-iperf-client.log
+
+killall iperf
+sudo pkill cubic
+
+echo "Showing end of syslog"
+tail /var/log/syslog -n 75
+
+popd
