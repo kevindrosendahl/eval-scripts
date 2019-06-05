7369977916e3370b0fb3173c911ee71e1373132f
Kevin Rosendahl <kevindrosendahl@gmail.com>
2019-06-05 14:39:20 -0700

    use proper color scheme for stacked figures

--- reproduction/scalability/plot/stacked.r
+++ reproduction/scalability/plot/stacked.r
@@ -6,8 +6,15 @@ library(plyr)
 args <- commandArgs(trailingOnly=TRUE)
 data <- read.csv(args[1], sep=" ")
 
-ggplot(data) +
+ggplot(data, aes(x=factor(NumFlows), y=Value, fill=Type)) +
     geom_bar(aes(x=factor(NumFlows), y=Value, fill=Type), stat="identity") +
+    scale_fill_manual(
+        values=c(
+            "user" = "#88419d",
+            "system" = "#8c96c6",
+            "interrupt" = "#b3cde3"
+        )
+    ) +
     labs(x="Flows", y="CPU Utilization %") + 
     facet_wrap(~Impl)
 
