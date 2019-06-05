b860b140e708333df9eeaa617528633493da1fa2
Kevin Rosendahl <kevindrosendahl@gmail.com>
2019-06-05 02:16:22 -0700

    update averaged theme

--- reproduction/scalability/plot/averaged.r
+++ reproduction/scalability/plot/averaged.r
@@ -9,15 +9,19 @@ data <- read.csv(args[1], sep=" ")
 ggplot(data, aes(x=factor(NumFlows), y=Average, fill=Impl)) +
     geom_col(aes(x=factor(NumFlows), y=Average, fill=Impl), position="dodge") +
     geom_errorbar(aes(ymin=Average-StdDev,ymax=Average+StdDev), position="dodge") +
-    facet_wrap(~Alg) +
-    labs(x="Flows", y="CPU Utilization %")
-
-
-# ggplot(summarized, aes(x=factor(NumFlows), y=m, fill=Impl)) +
-#     geom_col(aes(x=factor(NumFlows), y=m, fill=Impl)) +
-#     geom_errorbar(aes(ymin=m-sd,ymax=m+sd)) +
-#     facet_wrap(~Impl) +
-#     labs(x="Flows", y="CPU Utilizaiton %") 
+    scale_fill_manual(
+        labels=c(
+            "ccp" = "ccp",
+            "kernel" = "kernel"
+        ),
+        values=c(
+            "ccp" = "#88419d",
+            "kernel" = "#b3cde3"
+        ),
+        guide=guide_legend(title=NULL)
+    ) +
+    labs(x="Flows", y="CPU Utilization %") +
+    facet_wrap(~Alg)
 
 ggsave(args[2], width=6, height=2)
 
