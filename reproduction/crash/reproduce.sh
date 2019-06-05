641c686c69ff224840a2613dec42eefa33bd8117
Kevin Rosendahl <kevindrosendahl@gmail.com>
2019-06-05 16:47:58 -0700

    update crash reproduce

--- reproduction/crash/reproduce.sh
+++ reproduction/crash/reproduce.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${DIR}/../setup.sh
 
 echo "Starting cubic ccp implementation, logging to ~/crash-cubic-ccp.log"
-sudo ./generic-cong-avoid/target/release/cubic --ipc=netlink --deficit_timeout=20 > ~/crash-cubic-ccp.log 2>&1 &
+sudo ${DIR}/../../generic-cong-avoid/target/release/cubic --ipc=netlink --deficit_timeout=20 > ~/crash-cubic-ccp.log 2>&1 &
 
 sleep 1
 
@@ -13,7 +13,7 @@ echo "Starting iperf server, logging to ~/crash-iperf-server.log"
 iperf -s -p 4242 > ~/crash-iperf-server.log 2>&1 &
 
 echo "Starting iperf client, logging to ~/crash-iperf-client.log"
-iperf -c localhost -p 4242 -Z ccp -t 1 2>&1 | tee ~/crash-iperf-client.log
+iperf -c localhost -p 4242 -Z ccp 2>&1 | tee ~/crash-iperf-client.log
 
 killall iperf
 sudo pkill cubic
