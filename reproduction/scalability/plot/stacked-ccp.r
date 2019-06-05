efbc51ef5201920f513210f0062e6cd40ea888b6
Kevin Rosendahl <kevindrosendahl@gmail.com>
2019-06-05 16:22:13 -0700

    add stacked ccp

---
+++ reproduction/scalability/plot/stacked-ccp.r
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+library(ggplot2)
+library(plyr)
+
+args <- commandArgs(trailingOnly=TRUE)
+data <- read.csv(args[1], sep=" ")
+
+ggplot(data) +
+    #geom_col(aes(x=factor(NumFlows), y=Value, fill=Impl), position="dodge") +
+    geom_bar(aes(x=factor(NumFlows), y=Value, fill=Type), stat="identity") +
+    scale_fill_manual(
+        values=c(
+            "user" = "#88419d",
+            "system" = "#8c96c6",
+            "interrupt" = "#b3cde3"
+        )
+    ) +
+    labs(x="Flows", y="CPU Utilization %") +
+    facet_wrap(~Impl)
+
+ggsave(args[2], width=6, height=2)
+
